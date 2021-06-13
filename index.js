e5e37146212d774878820f45a8faa18579ef0b5d
Wilson Diaz <wdfiguereo@gmail.com>
2021-06-13 02:34:59 -0400

    3.10: heroku procfile

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('postBody', (request) => {
   if (request.method === 'POST') {
