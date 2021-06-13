4541062549782785bc678fd67f2bfde82668771c
Wilson Diaz <wdfiguereo@gmail.com>
2021-06-13 02:40:25 -0400

    3.10: use env port

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ app.post('/api/persons', (request, response) => {
   response.json(newPerson)
 })
 
-app.listen(3001, () => {
-  console.log("server running on port 3001")
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
