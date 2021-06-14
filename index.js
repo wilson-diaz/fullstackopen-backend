40853ea39ebdf937a719a953fcdeda3cd4226867
Wilson Diaz <wdfiguereo@gmail.com>
2021-06-14 00:22:15 -0400

    3.15-16: deleting and error handling

--- index.js
+++ index.js
@@ -24,19 +24,19 @@ app.get('/info', (request, response) => {
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(result => {
     response.json(result)
-  })
+  }).catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(result => {
     response.json(result)
-  })
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  persons = persons.filter(p => p.id !== Number(request.params.id))
-  console.log(persons)
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id).then(result => {
+    response.status(204).end()
+  }).catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -58,8 +58,26 @@ app.post('/api/persons', (request, response) => {
 
   newPerson.save().then(result => {
     response.json(result)
-  })
+  }).catch(error => next(error))
 })
+
+// handle requests to unknown endpoints
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+app.use(unknownEndpoint)
+
+// error handling
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
